aa4f51894f1cb93aae5bcde30b39a2c4a01c97bd
Justin Calleja <calleja.justin@gmail.com>
2016-08-07 20:19:56 +0200

    Can just require current dir in tryme (main script is picked up from package.json)

--- tryme.js
+++ tryme.js
@@ -1,5 +1,5 @@
 // require('@justinc/yesno') elsewhere
-require('./src')({ message: 'Do you want to do X?' }).then(answer => {
+require('.')({ message: 'Do you want to do X?' }).then(answer => {
   if (answer.yes) {
     console.log('Doing X');
   } else {
